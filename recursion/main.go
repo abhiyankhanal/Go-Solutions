c74b78b16ecc3913e154d7072cf6110b764d929a
abhiyankhanal <abhiyan5588@gmail.com>
2022-11-11 15:09:22 +0545

    efficient code

--- recursion/main.go
+++ recursion/main.go
@@ -3,18 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	input := 3
+	input := 6
 	result := recursion(input)
 	println(result)
 }
 
 func recursion(input int) (result int) {
-	if input < 0 {
-		fmt.Println("The number must be positive")
-		return 0
-	} else if input <= 1 {
+	if input > 1 {
+		return (input * recursion(input-1))
+	} else if input >= 0 {
 		return 1
 	} else {
-		return (input * (input - 1))
+		fmt.Println("The number must be positive")
+		return 0
 	}
 }
